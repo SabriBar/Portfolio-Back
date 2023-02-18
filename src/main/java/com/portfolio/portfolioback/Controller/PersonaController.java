445e6c38aafebad6d2bcd0e8451aa06a5b7cc3fe
SabriBar <sabrinabaraldo@hotmail.com>
2023-02-18 10:01:11 -0300

    metodo personas crear

--- src/main/java/com/portfolio/portfolioback/Controller/PersonaController.java
+++ src/main/java/com/portfolio/portfolioback/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,12 @@ public class PersonaController {
     @Autowired
     ImpPersonaService impPersonaService;
     
+    @PostMapping("/crear")
+    public String createPersona(@RequestBody Persona persona){
+        impPersonaService.save(persona);
+        return "Persona creada correctamente";
+    }
+
     @GetMapping("/lista")
     public ResponseEntity<List<Persona>> list() {
         List<Persona> list = impPersonaService.list();
